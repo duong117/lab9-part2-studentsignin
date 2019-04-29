d21a85e01bb2c2278817d801f9b02885ef9f7a26
duong117 <47869833+duong117@users.noreply.github.com>
2019-04-29 14:23:41 -0500

    update

--- server.js
+++ server.js
@@ -2,21 +2,37 @@ var express=require('express')
 var bodyParser=require('body-parser')
 var Sequelize=require('sequelize')
 var api_routes=require('./routes/api.js')
+var path = require ('path')
 
-sequelize=new Sequelize({
-    dialect:'sqlite',
-    storage:'./db.sqlite3'
-})
+db_url=process.env.Database_URL
 
-sequelize.authenticate()
-    .then(()=> console.log('connected to sqlite'))
-    .catch(err => console.log('error connecting',err))
+let sequelize
 
+if (db_url) {
+    sequelize = new Sequelize(db_url, {
+        dialect: 'postgres',
+    })
+
+    sequelize.authenticate()
+        .then(() => console.log('connected to Postgres'))
+        .catch(err => console.log(err))
+}
+else {
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: './db.sqlite3'
+    })
+    sequelize.authenticate()
+        .then(() => console.log('connected to sqlite'))
+        .catch(err => console.log('error connecting', err))
+
+}
 let student=require('./model/student.js')(sequelize,Sequelize)
 
 var app=express()
 app.use(bodyParser.json())
 
+app.use(express.static(path.join(_dirname,'Student-sign-in-client','dist')))
 app.use('/api',api_routes(student))
 
 app.use(function(req,res,next){
